ff5d36f71e93556b01005a559f80011615a770b7
Denis <denislopesmoura@gmail.com>
2019-11-19 22:53:11 -0300

    adição de pushbutton para testar lampada

--- main.ino
+++ main.ino
@@ -9,25 +9,54 @@
 
 #define pinRele 7
 #define pinPIR 6
+#define pinButton 5
+
+int statusButton = 0;
 
 
 void setup()
 {
     Serial.println(9600);
-    Serial.println("Iniciando Detecção")
+    Serial.println("Iniciando detecção...\n");
 
+    //configura os pinos do rele, sensor de presença e Push Button
     pinMode(pinRele, OUTPUT);
     pinMode(pinPIR, INPUT);
+    pinMode(pinButton, INPUT);
 
     //garante que a lampada iniciará apagada
     digitalWrite(pinRele, LOW);
+    digitalWrite(pinButton, LOW);
 
 }
 
+void pushButton()
+    {
+        
+        //lê pino do botão
+        statusButton = digitalRead(pinButton);
+
+        if(statusButton == HIGH)
+        {   
+            //Desliga a lâmpada
+            digitalWrite(pinRele, LOW);
+        }
+        else
+        {
+            //Liga a lãmpada
+            digitalWrite(pinRele, HIGH);
+        }
+        
+    }
+    
 void loop()
 {
-    //lê o sensor PIR
+    //lê o sensor PIR e o PushButton
     bool valorPIR = digitalRead(pinPIR);
+    int statusButton = digitalRead(pinButton);
+
+    //controla botão de teste para ligar/desligar lâmpada
+    pushButton(statusButton);
 
     if(valorPIR)
     {
